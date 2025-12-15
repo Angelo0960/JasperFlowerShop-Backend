3da601bc705231784188a77526e9fd5b35755a30
MATJANNUS15 <matjannuspo@gmail.com>
2025-12-15 20:03:43 +0800

    day 2 done. export&postroute

--- routers/SalesRouter.js
+++ routers/SalesRouter.js
@@ -8,3 +8,8 @@ import {
 } from "../controllers/SalesController.js";
 
 const router = express.Router();
+
+router.get("/", listSales);             
+router.get("/export", exportSales);     
+
+router.post("/", recordSale);   
