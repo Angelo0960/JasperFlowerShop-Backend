8c0de10e46a1ea81d37ab698082d007b0b4119ad
Angelo0960 <patalangelo0960@gmail.com>
2025-12-15 23:58:29 +0800

    day 7 week 1 created and order and product model done

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -20,4 +20,59 @@ export default class ProductModel {
         const [rows] = await pool.query(query, params);
         return rows;
     }
+        static async getProductById(id) {
+        const [rows] = await pool.query("SELECT * FROM products WHERE id = ?", [id]);
+        return rows[0] || null;
+    }
+
+    static async createProduct(productData) {
+        const { 
+            product_code, 
+            name, 
+            description, 
+            category, 
+            unit_price
+        } = productData;
+        
+        const [result] = await pool.query(
+            `INSERT INTO products (product_code, name, description, category, unit_price)
+             VALUES (?, ?, ?, ?, ?)`,
+            [product_code, name, description, category, unit_price]
+        );
+        
+        return {
+            id: result.insertId,
+            ...productData
+        };
+    }
+
+    static async updateProduct(id, productData) {
+        const { 
+            name, 
+            description, 
+            category, 
+            unit_price
+        } = productData;
+        
+        const [result] = await pool.query(
+            `UPDATE products 
+             SET name = ?, description = ?, category = ?, unit_price = ?, updated_at = CURRENT_TIMESTAMP
+             WHERE id = ?`,
+            [name, description, category, unit_price, id]
+        );
+        
+        return result.affectedRows > 0;
+    }
+
+   
+    static async deleteProduct(id) {
+        const [result] = await pool.query(
+            `UPDATE products 
+             SET is_active = FALSE, updated_at = CURRENT_TIMESTAMP
+             WHERE id = ?`,
+            [id]
+        );
+        
+        return result.affectedRows > 0;
+    }
 }
