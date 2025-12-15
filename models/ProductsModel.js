23953103ce08556bc51d6eec71ee998c97b5bcbd
Angelo0960 <patalangelo0960@gmail.com>
2025-12-15 23:56:23 +0800

    day 6 week 1 created

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,1 +1,23 @@
 import pool from "./db.js"
+
+export default class ProductModel {
+ static async getProducts(filters = {}) {
+        let query = `SELECT * FROM products WHERE is_active = TRUE`;
+        const params = [];
+        
+        if (filters.category) {
+            query += ` AND category = ?`;
+            params.push(filters.category);
+        }
+        
+        if (filters.search) {
+            query += ` AND (name LIKE ? OR description LIKE ? OR product_code LIKE ?)`;
+            params.push(`%${filters.search}%`, `%${filters.search}%`, `%${filters.search}%`);
+        }
+        
+        query += ` ORDER BY name`;
+        
+        const [rows] = await pool.query(query, params);
+        return rows;
+    }
+}
